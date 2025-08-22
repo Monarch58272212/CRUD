443cf72b8a49af7b8b1acb9410861e8f523a62fd
Monarch <gusoaxcell@example.com>
2025-08-23 07:11:01 +0800

    1

--- app/FetchAll/page.tsx
+++ app/FetchAll/page.tsx
@@ -1,20 +1,56 @@
 "use server";
-import { Avatar, Button, Flex, SimpleGrid, Text } from "@chakra-ui/react";
-import prisma from "../lib/prisma";
+import { Button, Flex, SimpleGrid, Text, useToast } from "@chakra-ui/react";
+
 import Image from "next/image";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { Product } from "@prisma/client";
 
-export default async function Page() {
-  const data = await prisma.product.findMany();
+import { useEffect, useState } from "react";
 
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+interface Product {
+  id: string;
+  imageURL: string;
+  name: string;
+  price: number;
+  createdBy: string;
+}
+
+export default function Page() {
+  const [products, setProduct] = useState<Product[]>([]);
+  const toast = useToast();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const req = await fetch(`/api/products`);
+        if (!req.ok) {
+          throw new Error("Failed to fetch products");
+        }
+        const data = await req.json();
+
+        setProduct(data);
+        toast({
+          title: "Products fetched successfully",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        toast({
+          title: "Error fetching products",
+          description: "Please try again later.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    }
+    fetchData();
+  }, [toast]);
 
   return (
     <Flex flexDir={"column"} w={"100%"} justify={"center"} align={"center"}>
       <SimpleGrid columns={[1, 2, 3, 4]} spacing={4}>
-        {data.map((e: Product) => (
+        {products.map((e: Product) => (
           <Flex key={e.id} flexDir={"column"} align={"center"} gap={2}>
             <Image
               src={e.imageURL || "/default.jpg"}
@@ -23,11 +59,7 @@ export default async function Page() {
               layout="responsive"
               alt={e.name}
             />
-            {user?.id === e.createdBy ? (
-              <Avatar src={user?.picture || undefined} />
-            ) : (
-              <Avatar src="/logo.png" />
-            )}
+
             <Text> Name: {e.name}</Text>
             <Text>Price: {Number(e.price)}</Text>
             <Flex gap={2}>
