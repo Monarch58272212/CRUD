90ec0d6b13a019075b27e523f5f433cc135baadc
Monarch <gusoaxcell@example.com>
2025-08-23 07:33:30 +0800

    4

--- app/FetchAll/page.tsx
+++ app/FetchAll/page.tsx
@@ -2,7 +2,6 @@ import { Avatar, Button, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import prisma from "../lib/prisma";
 import Image from "next/image";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { Product } from "@prisma/client";
 
 export default async function Page() {
   const data = await prisma.product.findMany();
@@ -13,7 +12,7 @@ export default async function Page() {
   return (
     <Flex flexDir={"column"} w={"100%"} justify={"center"} align={"center"}>
       <SimpleGrid columns={[1, 2, 3, 4]} spacing={4}>
-        {data.map((e: Product) => (
+        {data.map((e) => (
           <Flex key={e.id} flexDir={"column"} align={"center"} gap={2}>
             <Image
               src={e.imageURL || "/default.jpg"}
