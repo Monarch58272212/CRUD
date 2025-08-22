98a2ab03b5c3e19fb4373b9975c6e4ff02c42d92
Monarch <gusoaxcell@example.com>
2025-08-23 07:30:36 +0800

    3

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+  reactStrictMode: false, // disables React Strict Mode
+  eslint: {
+    ignoreDuringBuilds: true, // disables ESLint during build
+  },
   images: {
     domains: ["images.unsplash.com", "plus.unsplash.com"],
   },
